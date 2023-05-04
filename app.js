21e911c2658323ac1a586baa0b614227f55b5b69
Alex8943 <65241585+Alex8943@users.noreply.github.com>
2023-05-04 09:04:06 +0200

    search and add user is done

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require("body-parser"); //import body-parser
 const indexRouter = require("./controllers/index"); //import index.js from controllers folder
-
+const authorRouter = require("./controllers/authors"); //import authors.js from controllers folder
 
 app.set("view engine", "ejs"); // set view engine to ejs, so we can use ejs files
 app.set("views", __dirname + "/views"); //tell where to find the views
@@ -14,6 +15,7 @@ app.set("layout", "layouts/layout"); //tell where to find the layout file, such 
 
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false}));
 
 //connect to mongodb
 const mongoose = require("mongoose");
@@ -24,6 +26,7 @@ db.on("error", error => console.error(error));
 db.once("open" , () => console.log("Connected to Mongoose"));
 
 
-
 app.use("/", indexRouter); //use index.js from routes folder (imported above")
+app.use("/authors", authorRouter); //use authors.js from routes folder (imported above")
+
 app.listen(3000, () => console.log('Server running on port 3000')); //
